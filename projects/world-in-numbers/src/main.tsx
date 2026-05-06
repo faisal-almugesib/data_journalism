069c1cba92d918c3a63024f7dcf410ba3aa7d00b
faisal <faisal.almugesib@gmail.com>
2026-05-06 11:09:22 +0300

    .

--- projects/world-in-numbers/src/main.tsx
+++ projects/world-in-numbers/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Navigate to="/phase-1" replace />} />
         <Route path="/phase-1" element={<Phase1 />} />
-        {/* Phase 2 & 3 will be added later */}
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
